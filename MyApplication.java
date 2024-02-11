f9502afe25a569a7b25ba1deee07efee3af4f22b
Hemu2644 <61935557+Hemu2644@users.noreply.github.com>
2024-02-11 21:26:02 +0530

    bugs found

--- MyApplication.java
+++ MyApplication.java
@@ -2,5 +2,6 @@ public class MyApplication {
 
     public static void main(String[] args) {
         System.out.println("Welcome to Git Learning");
+        System.out.println("2 more Bugs Found!");
     }
 }
